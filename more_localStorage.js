d03b8e2c4a5003462d628411038e99e7f22937d3
sandrorios <sandroderios@hotmail.com>
2023-12-06 22:27:03 -0500

    store and retrieve data from local stroage

--- more_localStorage.js
+++ more_localStorage.js
@@ -1,5 +1,21 @@
 // Store an objec in Local Storage
-localStorage.setItem('user', JSON.stringify({name: 'Gopi Gorantala', age: '32'}))
+// localStorage.setItem('user', JSON.stringify({name: 'Gopi Gorantala', age: '32'}))
 
 // Retrieve an ogject in Local Storage
-const user = JSON.parse(localStorage.getItem('user'));
+// const user = JSON.parse(localStorage.getItem('user'));
+
+const myObj = {
+    name: "John",
+    ageg: 31,
+    city: "New York"
+};
+
+
+// Storing data:
+const myJSON = JSON.stringify(myObj);
+localStorage.setItem("testJSON", myJSON);
+
+// Retrieving data:
+let text = localStorage.getItem("testJSON");
+let obj = JSON.parse(text);
+document.getElementById('demo').innerHTML = obj.name;
